3abdc10b8987e8cc19f9097b9a07028c59f9df35
jihunglin <t256413@gmail.com>
2019-07-31 11:22:18 +0800

    [更新] 加入register interface

--- controller/videoSocket_con.py
+++ controller/videoSocket_con.py
@@ -7,6 +7,28 @@ from flask import Blueprint
 videoStream = Blueprint(r'videoStream', __name__, url_prefix=r'/videoStream')
 
 
+class Message:
+    def __init__(self, status: int, message: str):
+        self.status = status
+        self.message = message
+
+
+@videoStream.route('/register')
+def registerUser(socket):
+    message = socket.receive()
+    try:
+        userName = message['name']
+        userDescription = message['userDescription']
+        if ~(len(userDescription) > 0) or ~(len(userName) > 0):
+            message = Message(1, 'userName and userDescription cannot be empty!')
+            socket.send(message.__dict__)
+            return
+        else:
+            return
+    except Exception as e:
+        socket.send('Error Input')
+
+
 @videoStream.route('/upload')
 def echo_socket(socket):
     while not socket.closed:
@@ -19,7 +41,6 @@ def echo_socket(socket):
             # cv2.imshow('.123', image)
             # cv2.imwrite('output.jpg', image)
             socket.send(str(image.shape))
-            print("======================")
         except Exception as e:
             socket.send(str(e))
             print(e)
